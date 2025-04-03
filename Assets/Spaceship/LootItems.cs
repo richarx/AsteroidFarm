2f00a6b532f29794a4bdb2aea02ebeada6f47a1c
richarx <theorichard12@gmail.com>
2025-04-03 19:08:16 +0200

    Loot vfx

--- Assets/Spaceship/LootItems.cs
+++ Assets/Spaceship/LootItems.cs
@@ -5,11 +5,13 @@ namespace Spaceship
     public class LootItems : MonoBehaviour
     {
         [SerializeField] private SqueezeAndStretch squeezeAndStretch;
+        [SerializeField] private GameObject lootVfx;
     
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.CompareTag("Loot"))
             {
+                Instantiate(lootVfx, other.transform.position, Quaternion.identity);
                 squeezeAndStretch.Trigger();
                 Destroy(other.gameObject);
             }
